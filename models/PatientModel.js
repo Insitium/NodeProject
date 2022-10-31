50a62e2c42d76ada07126049015cdd4b495b45a2
Satender Yadav <satenderyadav@Sams-Machine.local>
2022-10-31 18:12:52 -0400

    created-model

--- models/PatientModel.js
+++ models/PatientModel.js
@@ -3,14 +3,10 @@ const Schema = mongoose.Schema;
 
 //model of the patients data
 const patientSchema = new mongoose.Schema({
-    "fullName": String,
-    "id":{
-        type: String,
-        unique: true
-    },
-    age: String,
-    phoneNumber: String
+    fullName: { type: String, required: true },
+    age: { type: String, required: true },
+    address: { type: Date, required: true },
+    dob: { type: Date, required: true }
 })
 
-
-module.exports = mongoose.model("patients",patientSchema)
+module.exports = mongoose.model("patient",patientSchema)
